11bc0ad3b35ff35ec2321bfc85c9de98f6654d1d
Wei Zhang <macctown@hotmail.com>
2018-04-26 18:53:01 -0400

    Update BaseMetaData.sol

--- BaseMetaData.sol
+++ BaseMetaData.sol
@@ -2,5 +2,15 @@ pragma solidity ^0.4.0;
 
 import './MetaData';
 contract BaseMetaData is MetaData{
-  //common function
+  
+  string flags;
+  address deviceId;
+  string deviceType;
+  string deviceVersion;
+  
+  function BaseMetaData(string flags) public {
+    
+  }
+  
+  
 }
